fa499cbb8fbbb8965d8e027295842ab77775b45b
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2024-05-27 14:01:59 +0100

    tested variable names

--- Variable_naming.py
+++ Variable_naming.py
@@ -1,7 +1,7 @@
 print ("hello world")
 # VARIABLE NAMING CONVENTION
 # snake case : where all the letters in your variables must be small letters.
-My_friend= "Kifasi"
+my_friend= "Kifasi"
 
 # camel case: write the first word with small letters then the subsequent words will start with capital letter
 myFriend = "kifasi"
@@ -11,3 +11,13 @@ MyFriend = "true"
 
 # Constant case are used only in python where all the words are written in CAPSLOCK but separated by _ 
 MY_FRIEND = "KIsaa"
+
+# TEST 
+# Create 7 variables using Snake Case
+his_name = "Adex"
+her_name = "Alex"
+movie_name = "Damsel"
+series_name = "You"
+shoe_name = "Kapusu"
+
+print (his_name, her_name, movie_name, series_name, shoe_name)
