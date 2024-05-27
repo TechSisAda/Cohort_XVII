e2528332f5ccfa6b258cd93da0d6a4e0b1218132
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2024-05-27 14:13:36 +0100

    functions folder

---
+++ functions.py
@@ -0,0 +1,9 @@
+# bunch of code designed to define your code action 
+# TYPES OF FUCTIONS
+# BUILT IN - come in directly with python
+# USER-DEFINED - functions you create yoursdelf
+# IMPORT - FXNs you import from  a separate code 
+
+#BUILT-IN 
+print()
+
