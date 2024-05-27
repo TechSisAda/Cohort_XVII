b85e76d25bf879d6c80a5fcfa6b80e5b1c7ce178
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2024-05-27 13:52:27 +0100

    changed name

---
+++ Variable_naming.py
@@ -0,0 +1,13 @@
+print ("hello world")
+# VARIABLE NAMING CONVENTION
+# snake case : where all the letters in your variables must be small letters.
+My_friend= "Kifasi"
+
+# camel case: write the first word with small letters then the subsequent words will start with capital letter
+myFriend = "kifasi"
+
+# Pascal Case: writes the words with their first letters capitalized
+MyFriend = "true"
+
+# Constant case are used only in python where all the words are written in CAPSLOCK but separated by _ 
+MY_FRIEND = "KIsaa"
--- helloworld.py
+++
@@ -1,4 +0,0 @@
-print ("hello world")
-# VARIABLE NAMING CONVENTION
-# snake case : where all the letters in your variables must be small letters.
-My_friend= "Kifasi"
