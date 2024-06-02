e56fd4f272be2ffce176edc07ccc8947ff89cb6f
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2024-06-02 15:46:08 +0100

    renamed files

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-adding a none empty readme 
 <h2>AUTHOR :</h2>
 <p>NNONYELU ADAEZE </p>
 TECHSISADA@YAHOO.COM
---
+++ adaeze_nnonyelu.py
@@ -0,0 +1,13 @@
+# name_input = str(input("Enter your name (Surname first): \n"))
+# bank_input = str(input("Enter Bank name: \n"))
+# amount_input = int(input("How much do you intend to loan? \n"))
+# loan_rate = input("How much rate you intend " + bank_input + " to loan you? \n")
+# loan_duration = input("How long is this loan valid for ? \n")
+# total_payback = 
+# agreement = 
+# print(name_input.title())
+#print(bank_input.upper())
+
+# rate = float(loan_rate) / 100 * amount_input
+# print(rate)
+
